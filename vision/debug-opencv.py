c20032fc4be53d62104e725d3fa17b71771b5435
Zsolt Ero <s0956379@inishail.inf.ed.ac.uk>
2012-03-10 20:33:30 +0000

    vis-

--- vision/debug-opencv.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import cv
-from SimpleCV import *
-
-file = "debug.jpg"
-img = Image(file)
-
-img = img.crop(x=38,y=95,w=648,h=392)
-mask_red = img.colorDistance( color=(208, 28, 22) ).binarize(88)   
