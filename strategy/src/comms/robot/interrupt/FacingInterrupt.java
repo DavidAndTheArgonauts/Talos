d79c614c81409bebfa7c6475e52bfb56363b9480
Gerald Chau <mwoit@ymail.com>
2012-03-20 02:17:55 +0000

    Removed the /2 on calculations

--- strategy/src/comms/robot/interrupt/FacingInterrupt.java
+++ strategy/src/comms/robot/interrupt/FacingInterrupt.java
@@ -6,6 +6,7 @@ public class FacingInterrupt extends AbstractInterrupt
 {
 	
 	private long lWheelRev = -1, rWheelRev = -1;
+	private boolean VERBOSE = false;
 	
 	public FacingInterrupt(int id, double value)
 	{
@@ -14,7 +15,7 @@ public class FacingInterrupt extends AbstractInterrupt
 	
 	public boolean hasTriggered(Commander commander)
 	{
-		
+		// Initial setup
 		if (lWheelRev == -1)
 		{
 			lWheelRev = commander.getLeftRevolution();
@@ -24,10 +25,12 @@ public class FacingInterrupt extends AbstractInterrupt
 			rWheelRev = commander.getRightRevolution();
 		}
 		
-		// wheel diff
+		// Wheel Difference
 		long lWheelDiff = commander.getLeftRevolution() - lWheelRev,
 			rWheelDiff = commander.getRightRevolution() - rWheelRev;
-		
+		if (VERBOSE)
+			System.out.println("lWheelDiff: " + lWheelDiff + ", rWheelDiff " + rWheelDiff);
+
 		long wheelUpdateTime = commander.getWheelUpdateTime();
 		
 		if (wheelUpdateTime > 0)
@@ -36,13 +39,22 @@ public class FacingInterrupt extends AbstractInterrupt
 			
 			lWheelDiff += commander.getLeftSpeed() * timeDiff;
 			rWheelDiff += commander.getRightSpeed() * timeDiff;
+			if (VERBOSE)
+			{
+				System.out.println("POST timeDiff: lWheelDiff: " + lWheelDiff + ", rWheelDiff " + rWheelDiff + " timeDiff: " + timeDiff);
+				System.out.println("commander.getLeftSpeed(): " + commander.getLeftSpeed() + " commander.getRightSpeed() " + commander.getRightSpeed());
+			}
 		}
 		
 		double turnDiff = ((double)rWheelDiff / 360.0) * Math.PI * Commander.WHEEL_DIAMETER - ((double)lWheelDiff / 360.0) * Math.PI * Commander.WHEEL_DIAMETER;
 		
-		double angleTurned = Math.toDegrees(turnDiff / Commander.WHEEL_SEPARATION)/2.0;
+		if (VERBOSE)
+			System.out.println("turnDiff: " + turnDiff);
+
+		double angleTurned = Math.toDegrees(turnDiff / Commander.WHEEL_SEPARATION);
 		
-		//System.out.println("angle turned = " + angleTurned + " [value = " + value + "] (lWheelDiff = " + lWheelDiff + ",rWheelDiff = " + rWheelDiff + ")");
+		if (VERBOSE1)
+			System.out.println("angle turned = " + angleTurned + " [value = " + value + "] (lWheelDiff = " + lWheelDiff + ",rWheelDiff = " + rWheelDiff + ")");
 		
 		if (value > 0 && angleTurned > value)
 		{
