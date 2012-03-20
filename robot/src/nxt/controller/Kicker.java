4c17812a81d9b87193c60aef17fb02d389075880
William Pond <w.pond@sms.ed.ac.uk>
2012-03-20 04:33:04 +0000

    Optimised kicker

--- robot/src/nxt/controller/Kicker.java
+++ robot/src/nxt/controller/Kicker.java
@@ -12,10 +12,14 @@ import java.lang.*;
 public class Kicker implements ConnectionInterface
 {
 	
+	private static long THREAD_TIMEOUT = 1000;	
+		
 	private Connection connection;
 	
 	private Thread kicker = null;
 	private Object lock = new Object();
+	
+	private long lastStart = -1;
 	
 	public Kicker(Connection connection)
 	{
@@ -29,28 +33,36 @@ public class Kicker implements ConnectionInterface
 	public synchronized void receiveCommand(Message msg)
 	{
 		
-		// Checks thread status
-		if (kicker != null && kicker.isAlive())
-			return;
-		
 		// Checks message broadcasted is for this class
 		if (msg.getOpcode() == Opcodes.KICK)
 		{
+			
+			// Checks thread status
+			if ((lastStart != -1 && System.currentTimeMillis() - lastStart < THREAD_TIMEOUT) && (kicker != null && kicker.isAlive()))
+				return;
 			
 			kicker = new Thread() {
 				
 				public void run()
 				{
 					
+//					Reset kicker to original position
 					Motor.A.setSpeed(900);
-					Motor.A.rotate(1, true);
+					Motor.A.backward();
 					try {
-						Thread.sleep(300);
+						Thread.sleep(50);
+					} catch (InterruptedException e) {
+					}
+					Motor.A.stop();
+					Motor.A.setSpeed(900);
+					Motor.A.forward();
+					try {
+						Thread.sleep(150);
 					} catch (InterruptedException e) {
 					}
 					// Reset kicker to original position
 					Motor.A.setSpeed(50);
-					Motor.A.rotate(-1, true);
+					Motor.A.backward();
 					try {
 						Thread.sleep(300);
 					} catch (InterruptedException e) {
@@ -62,6 +74,7 @@ public class Kicker implements ConnectionInterface
 			};
 			
 			kicker.start();
+			lastStart = System.currentTimeMillis();
 			
 		}
 		
