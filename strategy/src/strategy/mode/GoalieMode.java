2598704d90b3994931b8fe22849f4dcef8b00e4d
Gerald Chau <mwoit@ymail.com>
2012-04-02 00:58:18 +0100

    goalie mode

--- strategy/src/strategy/mode/GoalieMode.java
+++ strategy/src/strategy/mode/GoalieMode.java
@@ -30,10 +30,60 @@ public class GoalieMode extends AbstractMode
 			rUS = commander.getRightUltrasonic();
 		
 		
-			
-			
+		
+		System.out.println(lUS + rUS);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		int driveLeft = 0, driveRight = 0;
+        
+		int acceleration = 10;
+		int deceleration = 10;
+
+		if ( targetDriveLeft > prevDriveLeft + acceleration ) {
+		    driveLeft = prevDriveLeft + acceleration;
+		}
+		else if ( targetDriveLeft < prevDriveLeft - deceleration ) {
+		    driveLeft = prevDriveLeft - deceleration;
+		}
+		else {
+		    driveLeft = targetDriveLeft;
+		}
+	    
+		if ( targetDriveRight > prevDriveRight + acceleration ) {
+		    driveRight = prevDriveRight + acceleration;
+		}
+		else if ( targetDriveRight < prevDriveRight - deceleration ) {
+		    driveRight = prevDriveRight - deceleration;
+		}
+		else {
+		    driveRight = targetDriveRight;
+		}
 			
 		System.out.println("Ultrasonic values: [" + lUS + "," + rUS + "]");
+		System.out.println( "driveLeft: " + driveLeft + " driveRight: " + driveRight + "\n" );
+
+		if ( ControlGUI.paused ) {
+		    //commander.setSpeed( 0, 0);
+		    prevDriveLeft = 0;
+		    prevDriveRight = 0;
+		}
+
+		else {
+		    //commander.setSpeed( driveLeft, driveRight );
+		    prevDriveLeft = driveLeft;
+		    prevDriveRight = driveRight;
+	       }
 		
 	}
 	
